86e3d32b4bcb72afff0f8515558d32bf36ed41ff
Bhaskar p <pbhaskar870@gmail.com>
2022-10-03 22:55:45 +0530

    just welocme msg

--- src/components/TodoAddData.js
+++ src/components/TodoAddData.js
@@ -3,7 +3,14 @@ import React from 'react';
 
 const TodoAddData = () => {
   return (
-    <div>TodoAddData</div>
+    <div>
+        <center>
+            <h1>TodoAddData</h1>
+            <div>
+                <h2>welcome to toda app</h2>
+            </div>
+        </center>
+    </div>
   )
 }
 
