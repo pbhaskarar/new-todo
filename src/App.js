f98d356e119b68deb0b6a9e8832372b26d3de8eb
Bhaskar p <pbhaskar870@gmail.com>
2022-11-21 16:53:53 +0530

    newfiles added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import Home from './components/Home';
 import React from 'react'
-import {
-  Routes,
-  Route
-} from "react-router-dom";
+import {Routes,Route} from "react-router-dom";
 import Blog from './components/Blog'
 import Contact from './components/Contact'
 import Navbar from './components/Navbar'
