0c77d084f20668ac4d6557cb8cabd20f561f68b2
Bhaskar p <pbhaskar870@gmail.com>
2022-11-23 17:44:38 +0530

    new contact page added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,25 @@
+import { bgcolor } from '@mui/system'
 import React from 'react'
+import Checkout from './SignUp/Checkout'
+import SignUp from './SignUp/SignUp'
+
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <>
+     <div style={{bgcolor:"black"}}>
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-6'>
+            <SignUp />
+          </div>
+          <div className='col-md-6'>
+            <Checkout/>
+          </div>
+        </div>
+      </div>
+     </div>
+    </>
   )
 }
 
